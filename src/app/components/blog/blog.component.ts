c1e4a8af6507906e824699bc2fbd463d599c1834
Hüseyin Yürekli <20050634@ogrenci.ankara.edu.tr>
2023-06-12 15:43:02 +0300

    blog

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-blog',
-  templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  selector: "app-blog",
+  templateUrl: "./blog.component.html",
+  styleUrls: ["./blog.component.scss"],
 })
-export class BlogComponent {
+export class BlogComponent implements OnInit {
+  blogs!: any;
 
+  constructor(private router: Router, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getBlogs();
+  }
+
+  getBlogs() {
+    this.http.get<any>("assets/json/blogs.json").subscribe((data) => {
+      this.blogs = data;
+    });
+  }
+
+  goToDetailPage(name: any) {
+    this.router.navigate(["blog/" + name]);
+  }
 }
